ffaae58f807affe270c5ce9d1a188e5286d7917c
Mathis Gendron <mathis.gendron@etu.u-bordeaux.fr>
2018-11-10 18:42:11 +0100

    Definition fonction pour eliminer reference indefinies

--- game.c
+++ game.c
@@ -16,75 +16,107 @@ struct game_s {
 };
 
 
-game new_game_empty();
+game new_game_empty(){
+	game g=NULL;
+	return g;
+}
     // TO DO
    
 
-game new_game(piece *pieces, direction *initial_directions);
+game new_game(piece *pieces, direction *initial_directions){
+	game g=NULL;
+	return g;
+}
     // TO DO
  
 
-void set_piece(game g, int x, int y, piece piece, direction orientation);
+void set_piece(game g, int x, int y, piece piece, direction orientation){
+}
     // TO DO
 
 
-void shuffle_dir(game g);
+void shuffle_dir(game g){
+}
     // TO DO
 
 
-int game_height(cgame game);
+int game_height(cgame game){
+	return EXIT_SUCCESS;
+}
     // TO DO
   
 
- int game_width(cgame game);
+int game_width(cgame game){
+	return EXIT_SUCCESS;
+}
      // TO DO
 
 
-void rotate_piece_one(game game, int x, int y);
+void rotate_piece_one(game game, int x, int y){
+}
     // TO DO
 
 
-void rotate_piece(game game, int x, int y, int nb_cw_quarter_turn);
+void rotate_piece(game game, int x, int y, int nb_cw_quarter_turn){
+}
      // TO DO
 
 
-void set_piece_current_dir (game game, int x, int y, direction dir);
+void set_piece_current_dir (game game, int x, int y, direction dir){
+}
      // TO DO
 
 
-bool is_edge_coordinates(cgame g, int x, int y, direction dir);
+bool is_edge_coordinates(cgame g, int x, int y, direction dir){
+	return true;
+}
      // TO DO
 
 
 
-bool is_edge(piece piece, direction orientation, direction dir);
+bool is_edge(piece piece, direction orientation, direction dir){
+	return true;
+}
      // TO DO
 
 
-direction opposite_dir(direction dir);
+direction opposite_dir(direction dir){
+	return 0;
+} 
      // TO DO
 
 
-game copy_game (cgame g_src);
+game copy_game (cgame g_src){
+	game g=NULL;
+	return g;
+}
      // TO DO
 
 
-void delete_game (game g);
+void delete_game (game g){
+}
      // TO DO
 
 
-piece get_piece(cgame game, int x, int y);
+piece get_piece(cgame game, int x, int y){
+	return 0;
+}
      // TO DO
 
 
-direction get_current_dir(cgame g, int x, int y);
+direction get_current_dir(cgame g, int x, int y){
+	return 0;
+}
      // TO DO
 
 
-bool is_game_over (cgame g);
+bool is_game_over (cgame g){
+	return true;
+}
      // TO DO
 
 
-void restart_game(game g);
+void restart_game(game g){
+}
      // TO DO
 
--- game_io.c
+++ game_io.c
@@ -1,12 +1,19 @@
 #include "game.h"
 #include "game_io.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 
-game load_game(char* filename);
+
+game load_game(char* filename){
+	game g=NULL;
+	return g;
+}
     // TO DO
 
 
-void save_game(cgame g, char* filename);
+void save_game(cgame g, char* filename){
+	}
     // TO DO
 
     
