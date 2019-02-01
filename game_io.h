7cc9725ed71119e1fb15b5b9b3dae540955448a2
Alexandre Vialar <alexandre.vialar@etu.u-bordeaux.fr>
2019-02-01 12:14:44 +0100

    ajouté nouveau game_io.h

--- game_io.h
+++ game_io.h
@@ -6,11 +6,8 @@
  * @file game_io.h
  *
  * @brief This file provides functions to load or save a game.
- *  *** THE FILE FORMAT WILL BE PRESENTED LATER ***
  *
  **/
-
-///@{
 
 /**
  * @brief Creates a game by loading its description in a file
@@ -18,7 +15,7 @@
  * @param filename
  * @return the loaded game
  **/
-game load_game(char* filename);
+game load_game(char *filename);
 
 /**
  * @brief Save a game in a file
@@ -26,8 +23,7 @@ game load_game(char* filename);
  * @param g game to save
  * @param filename output file
  **/
-void save_game(cgame g, char* filename);
+void save_game(cgame g, char *filename);
 
-///@}
 
-#endif // GAME_IO_H
+#endif  // GAME_IO_H
