21b35c0f8d0ac503cb10d24900c9bfd8d440f318
Mathis Gendron <mathis.gendron@etu.u-bordeaux.fr>
2019-02-22 11:23:49 +0100

    "Added net_solve"

---
+++ net_solve.c
@@ -0,0 +1,14 @@
+#include "game.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "game_io.h"
+
+void usage(){
+    printf("Usage: net_solve FIND_ONE|NB_SOL|FIND_ALL <nom_fichier_pb> <prefix_fichier_sol>.\n");
+    exit(EXIT_FAILURE);
+}
+
+void main(int argc, char** argv){
+    
+}
