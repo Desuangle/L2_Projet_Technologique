12244ed4170b1cbfd63fae48f083199e30d6d669
walouini <walouini@u-bordeau.fr>
2019-02-27 10:47:42 +0100

    ajout des fonctions auxiliaire de la fonction load_game

--- game_io.c
+++ game_io.c
@@ -4,52 +4,47 @@
 #include <stdlib.h>
 #define MAX 409
 
-char* read_next_line1(FILE* p_f, uint* p_size){
-    char* s = malloc(MAX*sizeof(char));
-    if(s==NULL){return NULL;}
-    long old_pos=ftell(p_f), len = 0;
-    if(fgets(s,MAX,p_f)!=NULL){
-        len = ftell(p_f)-old_pos;
-        if(s[len-1]=='\n'){
-            s[len-1]='\0';
-            len--;
-        }else{ //feof return 0 if not end of file
-        if(!feof(p_f)){ //to detect error from last line without ’\n’
-            fprintf(stderr,"Line too long...");
-            exit(EXIT_FAILURE);
-        }
-        }
-        (*p_size)=len;
-        return s;
-    }
-    free(s);
-    return NULL; // in case of eof
-}
-
-long* convert_line1(char* line,uint* p_size){
-    long* tab = malloc((*p_size)*sizeof(long));
-    if(tab == NULL){ return NULL; }
-    long arr_s=0;
-    char * token = strtok(line," ");
-    while(token != NULL){
-        char* en = token;
-        long val = strtol(token, &en, 10);
-        if ((*en)=='\0') { // convert was successfull
-            tab[arr_s]=val;
-            arr_s++;
-        }else{
-            free(tab);
-            return NULL;
-        }
-        token = strtok(NULL, " ");
-    }
-    (*p_size)=arr_s;
-    return tab;
-}
 
 void error(char* message) {
 	fprintf(stderr, message);
 	exit(EXIT_FAILURE);
+}
+
+piece convert_piece(char tmp){
+	switch(tmp){
+	case 'L':
+		return LEAF;
+		break;
+	case 'S':
+		return SEGMENT;
+		break;
+	case 'C':
+		return CORNER;
+		break;
+	case 'T':
+		return TEE;
+		break;
+	case 'F':
+		return CROSS;
+		break;
+	}
+}
+
+direction convert_direction(char tmp){
+	switch(tmp){
+	case 'N':
+		return N;
+		break;
+	case 'S':
+		return S;
+		break;
+	case 'E':
+		return E;
+		break;
+	case 'W':
+		return W;
+		break;
+	}
 }
 
 game load_game(char* filename) {
@@ -62,31 +57,37 @@ game load_game(char* filename) {
         fprintf(stderr,"FILE ERR!\n");
         exit(EXIT_FAILURE);
     }
-	uint taille=0;
+	int width, height;
+	char tmp;
+	int err = fscanf(f,"%d %d %c", width, height, tmp);
+	if(err != 3)
+		error("Can't convert first line\n");
 	bool wrapping;
-	char* ligne=read_next_line1(f, &taille);
-	char tmp=ligne[taille-1];
-	char* endptr = ligne;
-	int width =(int)strtol(ligne, &endptr, 10);
-	//gestion d'erreur
-	int height =(int)strtol(ligne, &endptr, 10);
     if(tmp=='N')
 	    wrapping = false;
-        else
-        {
-            wrapping = true;
-        }
-        
+    else
+        wrapping = true;
+	
+	piece p[width*height];
+	direction d[width*height];
+	int i = 0;
+	tmp = fgetc(f); // on garde cette ligne si c'est une nouvelle ligne
+	while(tmp != EOF) {
+		tmp = fgetc(f);
+		p[i] = convert_piece(tmp);
+		tmp = fgetc(f);
+		d[i] = convert_direction(tmp);
+		tmp = fgetc(f);
+		i++;
+	}
 
-
-	game g = new_game_empty_ext(width,height,wrapping);
+	game g = new_game_ext(width, height, p, d, wrapping) ;
+	
     fclose(f);
 	return g;
 }
 
-=======
-}
-// TO DO
+
 void print_piece(FILE* savefile, piece p, direction d);
 
 void save_game(cgame g, char* filename) {
@@ -111,8 +112,7 @@ void save_game(cgame g, char* filename) {
 	fclose(savefile);
 }
 
-=======
-// TO DO
+
 
 void print_piece(FILE* savefile, piece p, direction d) {
 	switch(p){
@@ -154,4 +154,3 @@ void print_piece(FILE* savefile, piece p, direction d) {
 
 
     
->>>>>>> d561051fa546b3afbff09ac1ed16762a49b354c1
