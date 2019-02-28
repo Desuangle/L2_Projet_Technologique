52886253e62f19e9b6725cdb424cc5516dadb058
Killian Le Guen <kleguen@emi.u-bordeaux1.fr>
2019-02-28 14:20:58 +0100

    Fonction print

--- net_solve.c
+++ net_solve.c
@@ -9,14 +9,16 @@ void solver(cgame g, option o, char* filename) {
 	game g2 = copy_game(g);
 	int n = 0;
 	solver_rec(g2, o, 0, &n, filename);
-	return n;
+	//return n;
+	solver_print_nbsolv_or_no_sol(o,n,filename);
+
 }
 
 void solver_rec(game g, option o, int i, int* n, char* filename) {
 	int w = game_width(g), h = game_height(g);
 	if(i == w * h) {
-		if(is_game_over(g)) {
-			// solver_print(g, o, n, filename);
+		if(is_game_over(g)) {	
+		solver_print(g, o, n, filename);
 		}
 		return;
 	}
@@ -54,4 +56,45 @@ void solver_rec(game g, option o, int i, int* n, char* filename) {
 			solver_rec(g, o, i+1, n, filename);
 		}
 	}
+}
+
+void solver_print(game g,int *c,option opt,char filename)
+{
+	*c=*c+1;
+	if (opt == FIND_ONE)
+	{
+			save_game(c, g, prefixe);
+			return EXIT_SUCCESS;
+	}
+	else 
+	{
+		char *chaine;
+		sprintf(&chaine, "%s.sol%d", filename, *c);
+		save_game(c,g,chaine);
+	}
+}
+
+void solver_print_nbsolv_or_no_sol(option opt,int*c,filename)
+{
+	if ((opt == FIND_ONE || opt = FIND_ALL) && *c ==0)
+	{
+		create_file(filename,"NO SOLUTION")
+	}
+	else if (opt = NB_SOL)
+	{
+		char *chaine;
+		sprintf(&chaine, "NB_SOL = %d", *c);
+		create_file(filename,&chaine)
+	}
+}
+void create_file(char* filename, char* msg) {
+	FILE* savefile;
+	savefile = fopen(filename, "w");
+	if(!savefile)
+		error("Not enough memory\n");
+
+	fprintf(savefile, "%s\n", msg);
+	fclose(savefile);
+	}
+	
 }
