fc0ad9887530cae245bbba1f9f6054de20c2e1b2
Mathis Gendron <mathis.gendron@etu.u-bordeaux.fr>
2019-03-12 18:07:39 +0100

    Modification SIZE_CHAINE dans net_solve.h

--- net_solve.h
+++ net_solve.h
@@ -11,6 +11,6 @@ void solver(cgame g, option o, char* filename);
 void solver_rec(game g, option o, int i, int* n, char* filename);
 void solver_print(game g,int *c,option opt,char* filename);
 void solver_print_nbsolv_or_no_sol(option opt,int*c, char* filename);
-#define SIZE_CHAINE 16 // "NB_SOL = " : 9 characters + *c < 1,000,000 solutions : 6 + '\0' : 1 = 16
+#define SIZE_CHAINE 128 // "NB_SOL = " : 9 characters + *c < 1,000,000 solutions : 6 + '\0' : 1 = 16
 #define SIZE_PREFIX 128
 void create_file(char* filename, char* msg);
